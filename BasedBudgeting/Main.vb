d94f3312472eefc52745d84c2eceb467c174d717
MatthiasBenaets <matthias.benaets@gmail.com>
2021-12-18 11:49:01 +0100

    Python limitations, language switch

---
+++ BasedBudgeting/Main.vb
@@ -0,0 +1,69 @@
+﻿Imports System.IO
+Public Class App
+    Private Sub BtnBudget_Click(sender As Object, e As EventArgs) Handles btnBudget.Click       ' When Budget menu is selected
+        btnBudget.BackColor = Color.FromArgb(0, 90, 120)                                        ' Change Button Colors depending on selected menu
+        btnReports.BackColor = Color.FromArgb(45, 150, 175)
+        btnAccounts.BackColor = Color.FromArgb(45, 150, 175)
+        pnlBudgetStatistics.Visible = True                                                      ' Show correct panels for selected menu
+        pnlReportsStatistics.Visible = False
+        pnlAccountsTransaction.Visible = False
+        dgvTransactions.Visible = False
+    End Sub
+
+    Private Sub BtnReports_Click(sender As Object, e As EventArgs) Handles btnReports.Click     ' When Reports button is selected
+        btnBudget.BackColor = Color.FromArgb(45, 150, 175)                                      ' Change Button Colors depending on selected menu
+        btnReports.BackColor = Color.FromArgb(0, 90, 120)
+        btnAccounts.BackColor = Color.FromArgb(45, 150, 175)
+        pnlBudgetStatistics.Visible = False                                                     ' Show correct panels for selected menu
+        pnlReportsStatistics.Visible = True
+        pnlAccountsTransaction.Visible = False
+        dgvTransactions.Visible = False
+    End Sub
+
+    Private Sub BtnAccounts_Click(sender As Object, e As EventArgs) Handles btnAccounts.Click   ' When Accounts button is selected
+        btnBudget.BackColor = Color.FromArgb(45, 150, 175)                                      ' Change Button Colors depending on selected menu
+        btnReports.BackColor = Color.FromArgb(45, 150, 175)
+        btnAccounts.BackColor = Color.FromArgb(0, 90, 120)
+        pnlBudgetStatistics.Visible = False                                                     ' Show correct panels for selected menu
+        pnlReportsStatistics.Visible = False
+        pnlAccountsTransaction.Visible = True
+        dgvTransactions.Visible = True
+    End Sub
+
+    Private Sub App_Load(sender As Object, e As EventArgs) Handles MyBase.Load                  ' When applications loads
+        Me.lblDate.Text = DateTime.Now.ToString("MMM yyyy").ToUpper()                           ' Print correct date
+        Me.lblDateValue.Text = DateTime.Now.ToString("MMM yyyy").ToUpper()
+
+        Dim rows() As String
+        Dim values() As String
+        If My.Computer.FileSystem.FileExists(".\transactions.csv") Then                         ' If saved transaction file exists
+            rows = File.ReadAllLines(".\transactions.csv")
+            For i As Integer = 0 To rows.Length - 1 Step +1                                     ' Loop through all rows
+                values = rows(i).ToString().Split(",")                                          ' Split values at ","
+                Dim row(values.Length - 1) As String
+
+                For j As Integer = 0 To values.Length - 1 Step +1
+                    row(j) = values(j).Trim()
+                Next j
+
+                dgvTransactions.Rows.Add(row)                                                   ' Add row to datagridview
+            Next i
+        End If
+    End Sub
+
+    Private Sub App_Closed(sender As Object, e As EventArgs) Handles MyBase.Closed              ' When application closes
+        Dim writer As New StreamWriter(".\transactions.csv")
+        For i As Integer = 0 To dgvTransactions.Rows.Count - 2 Step +1                          ' Loop through all rows
+            For j As Integer = 0 To dgvTransactions.Columns.Count - 1 Step +1                   ' Loop through all columns within the row
+                If j = dgvTransactions.Columns.Count - 1 Then                                   ' Print values with ","; except last column
+                    writer.Write(dgvTransactions.Rows(i).Cells(j).Value)
+                Else
+                    writer.Write(dgvTransactions.Rows(i).Cells(j).Value & ",")
+                End If
+            Next j
+            writer.WriteLine("")                                                                ' Next line
+        Next i
+        writer.Close()
+    End Sub
+
+End Class
